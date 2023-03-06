a840657012b6825ba2684cc91842ab33a46b87af
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-06 18:14:34 -0300

    Update price.js

--- api/price.js
+++ api/price.js
@@ -1,1 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/price', async function(req, res) {
+  const price = await getCurrentPrice()
+  if (price) {
+    res.json({
+      price
+    })
+  } else {
+    res.status(404).send('Price not found')
+  }
+})
+
+app.listen(3000, function() {
+  console.log('API listening on port 3000')
+})
 
