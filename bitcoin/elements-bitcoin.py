daca5f7298af6fcaa01825d03fb06c87ada1769d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-22 06:42:06 -0300

    Update elements-bitcoin.py

--- bitcoin/elements-bitcoin.py
+++ bitcoin/elements-bitcoin.py
@@ -1,7 +1,7 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 def createHTLC(amount, recipient, preImage, secretHash, expiryTime, network):
-    rpc_connection = AuthServiceProxy("http://rpcuser:rpcpassword@localhost:8332")
+    rpc_connection = AuthServiceProxy("http://BitcoinFinane:rpcpassword@127.0.0.1:8332")
     if network == "Bitcoin Lightning":
         # Code to create an HTLC on the Bitcoin Lightning network using Bitcoin Core's RPC interface
         # ...
