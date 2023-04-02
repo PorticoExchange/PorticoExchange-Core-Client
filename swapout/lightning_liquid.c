704072aba8297319e83c1568b12726877f50e62a
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-04-02 10:25:27 -0300

    Update lightning_liquid.c

--- swapout/lightning_liquid.c
+++ swapout/lightning_liquid.c
@@ -1,1 +1,26 @@
+// Define the necessary functions for creating and verifying HTLCs
+void createHTLC(int amount, char* recipient, char* preImage, char* secretHash, int expiryTime, char* network) {
+  // Code to create an HTLC on the specified network
+  // ...
+}
 
+void verifyHTLC(int amount, char* recipient, int expiryTime, char* network) {
+  // Code to verify an HTLC on the specified network
+  // ...
+}
+
+// Define the swap function
+void performAtomicSwap(int amount, char* recipient, char* secretHash) {
+  // Generate an HTLC on the Bitcoin Lightning network
+  createHTLC(amount, recipient, nullptr, secretHash, 3600, "Bitcoin Lightning");
+
+  // Wait for recipient to claim the HTLC on the Lightning network
+
+  // Once the recipient has claimed the HTLC, generate an HTLC on the Liquid network
+  verifyHTLC(amount, recipient, 3600, "Liquid Bitcoin");
+
+  // Wait for the sender to claim the HTLC on the Liquid Network
+  createHTLC(amount, recipient, nullptr, secretHash, 3600, "Liquid Bitcoin");
+
+  // Once the sender has claimed the HTLC, the swap is complete and funds have been exchanged.
+}
