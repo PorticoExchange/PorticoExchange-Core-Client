e3e309bc91755f4d25ca63519dc236a3dd949135
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-05-22 07:02:31 -0300

    Update script.h

--- script/script.h
+++ script/script.h
@@ -1,4 +1,3 @@
-
 #ifndef PORTICO_SCRIPT_H
 #define PORTICO_SCRIPT_H
 
@@ -6,20 +5,38 @@
 #include <swap.h>
 #include <element.h>
 #include <bitcoin.h>
- 
-static const asset
-       
-static const swap 
- 
-static element 
 
-static bitcoin 
+static const asset someAsset;  // Define your asset variable here
 
-//script Portico base
-OP_IF
-       [HASHOP]  OP_EQUALVERIFY OP_DUP OP_HASH160             
-   OP_ELSE
-        [TIMEOUTOP] OP_DROP OP_DUP OP_HASH160 
-   OP_ENDIF
-   OP_EQUALVERIFY
-   OP_CHECKSIG
+static void performSwap()
+{
+    // Perform swap operations using the 'someAsset' variable
+    // ...
+}
+
+static void processElements()
+{
+    // Process elements using the 'someAsset' variable
+    // ...
+}
+
+static void handleBitcoin()
+{
+    // Handle Bitcoin operations using the 'someAsset' variable
+    // ...
+}
+
+// Script Portico base
+void porticoScript()
+{
+    OP_IF
+        [HASHOP] OP_EQUALVERIFY OP_DUP OP_HASH160
+    OP_ELSE
+        [TIMEOUTOP] OP_DROP OP_DUP OP_HASH160
+    OP_ENDIF
+    OP_EQUALVERIFY
+    OP_CHECKSIG
+}
+
+#endif // PORTICO_SCRIPT_H
+
