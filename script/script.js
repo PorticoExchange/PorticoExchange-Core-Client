7646205d6d0848d96b1b8d12cad20d13089ad9e7
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-19 08:37:39 -0300

    Update and rename script.h to script.js

---
+++ script/script.js
@@ -0,0 +1,39 @@
+const bitcoin = require('bitcoin-js-lib');
+const lihgnting = require('lightning-js-lib');
+
+
+class asset someAsset;  // Define your asset variable here
+
+class performSwap = {
+   asset: bitcoin
+   asset: lbtc
+   asset: seq
+{
+    
+}
+
+ class processElements = {
+{
+  process: bitcoin
+  process: lbtc
+  process: seq
+}
+
+class handleBitcoin = {
+{
+    // Handle Bitcoin operations using the 'someAsset' variable
+    // ...
+}
+
+// Script Portico base
+class porticoScript = {
+{
+    OP_IF
+        [HASHOP] OP_EQUALVERIFY OP_DUP OP_HASH160
+    OP_ELSE
+        [TIMEOUTOP] OP_DROP OP_DUP OP_HASH160
+    OP_ENDIF
+    OP_EQUALVERIFY
+    OP_CHECKSIG
+}
+
