bcdfd5e220d179347c8c3d72bfee2a42d3bd4f36
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-19 10:23:21 -0300

    Update liquid_lightning.js

--- swapint/liquid_lightning.js
+++ swapint/liquid_lightning.js
@@ -8,11 +8,9 @@ const script = require('script');
 function createHTLC(amount, recipient, preImage, secretHash, expiryTime, network) {
 // Implementation specific to the chosen network
 if (network === 'Bitcoin Testnet') {
-// Bitcoin Testnet implementation
-// ...
+function network = ('BitcoinTestnet'(
 } else if (network === 'Liquid Testnet') {
-// Liquid Testnet implementation
-// ...
+function network = ('LiquidTestnet')
 }
 }
 
