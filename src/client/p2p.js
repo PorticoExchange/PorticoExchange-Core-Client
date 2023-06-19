ca326442dfee3db2592dce6e57e2fdbebae86783
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-19 08:07:57 -0300

    Update and rename p2p.c to p2p.js

---
+++ src/client/p2p.js
@@ -0,0 +1,67 @@
+const bitcoin = require('bitcoinjs-lib');
+
+// Multisig escrow address class
+class MultisigEscrowAddress {
+  constructor(value, pubKey1, pubKey2) {
+    this.value = value;
+    this.pubKey1 = pubKey1;
+    this.pubKey2 = pubKey2;
+  }
+}
+
+// Function to create 2-of-2 multisig escrow address
+function createMultisigEscrowAddress(value, pubKey1, pubKey2) {
+  return new MultisigEscrowAddress(value, pubKey1, pubKey2);
+}
+
+// Seed random number generator
+const seed = Math.floor(Date.now() / 1000);
+const rng = new bitcoin.bip32.BIP32().fromSeed(Buffer.from(seed.toString(), 'utf-8'), bitcoin.networks.bitcoin);
+
+// Alice and Bob's public keys (dummy values)
+const alicePubKey = 'Alice\'s public key';
+const bobPubKey = 'Bob\'s public key';
+
+// Create 2-of-2 multisig escrow addresses
+const address1 = createMultisigEscrowAddress(20, alicePubKey, bobPubKey);
+const address2 = createMultisigEscrowAddress(20, bobPubKey, alicePubKey);
+
+// Initialize array of coins with random values
+const NUM_COINS = 10;
+const coins = [];
+for (let i = 0; i < NUM_COINS; i++) {
+  coins[i] = Math.floor(Math.random() * 10) + 1; // random value between 1 and 10
+}
+
+// Print the initial array of coins
+console.log('Initial coins:', coins.join(' '));
+
+// Perform coinswap algorithm
+let i = 0;
+let j = NUM_COINS - 1;
+while (i < j) {
+  // Alice sends coin to escrow address 1
+  if (coins[i] % 2 === 0) {
+    console.log(`Alice sends coin ${coins[i]} to escrow address 1`);
+    address1.value += coins[i];
+  }
+  // Bob sends coin to escrow address 2
+  if (coins[j] % 2 === 1) {
+    console.log(`Bob sends coin ${coins[j]} to escrow address 2`);
+    address2.value += coins[j];
+  }
+  const temp = coins[i];
+  coins[i] = coins[j];
+  coins[j] = temp;
+  i++;
+  j--;
+}
+
+// Alice and Bob each sign transaction to release coins from escrow
+console.log('Alice and Bob each sign transaction to release coins from escrow');
+const aliceShare = Math.floor(address1.value / 2);
+const bobShare = Math.floor(address2.value / 2);
+console.log(`Alice receives ${aliceShare} coins and Bob receives ${bobShare} coins`);
+
+// Print the final array of coins
+console.log('Final coins:', coins.join(' '));
