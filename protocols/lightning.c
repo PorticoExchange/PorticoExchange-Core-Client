30b12a4c3f1e54d55a42d9a3b055fee0862e073b
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-19 08:16:27 -0300

    Update lightning.c

--- protocols/lightning.c
+++ protocols/lightning.c
@@ -1,87 +1,104 @@
-#include <bitcoin/chainparams.h>
-#include <bitcoin/privkey.h>
-#include <bitcoin/pubkey.h>
-#include <bitcoin/script.h>
-#include <bitcoin/tx.h>
-#include <ccan/tal/str/str.h>
-#include <common/amount.h>
-#include <common/htlc_tx.h>
-#include <common/key_derive.h>
-#include <common/utils.h>
-#include <stdio.h>
-#include <wally_bip32.h>
+const bitcoin = require('bitcoinjs-lib');
+const lightning = require('lightning');
 
 /* Define the on-chain and Lightning network parameters */
-static const struct chainparams *chainparams = &chainparams_main;
-static const u32 locktime = 1573819200; /* 15 Nov 2019 */
+const chainparams = bitcoin.networks.bitcoin;
+const locktime = 1573819200; /* 15 Nov 2019 */
 
 /* Define the on-chain and Lightning network transaction outputs */
-static const struct amount_sat onchain_output = AMOUNT_SAT(500000);
-static const struct amount_msat lightning_output = AMOUNT_MSAT(1000000);
+const onchainOutput = 500000;
+const lightningOutput = 1000000;
 
-int main(void) {
-    /* Generate a random private key for the user */
-    struct privkey user_privkey;
-    randombytes_buf(&user_privkey, sizeof(user_privkey));
+async function main() {
+  /* Generate a random private key for the user */
+  const userKeyPair = bitcoin.ECPair.makeRandom({ network: chainparams });
+  const userPrivateKey = userKeyPair.privateKey;
 
-    /* Derive the corresponding public key for the user */
-    struct pubkey user_pubkey;
-    if (!pubkey_from_privkey(&user_privkey, &user_pubkey))
-        return 1;
+  /* Derive the corresponding public key for the user */
+  const userPublicKey = userKeyPair.publicKey;
 
-    /* Generate a random preimage for the HTLC */
-    struct sha256 preimage;
-    randombytes_buf(&preimage, sizeof(preimage));
+  /* Generate a random preimage for the HTLC */
+  const preimage = Buffer.from(bitcoin.crypto.sha256(Buffer.alloc(32)));
 
-    /* Compute the hash of the preimage */
-    struct sha256 hash;
-    sha256(&hash, &preimage, sizeof(preimage));
+  /* Compute the hash of the preimage */
+  const hash = bitcoin.crypto.sha256(preimage);
 
-    /* Construct the HTLC script for the on-chain transaction */
-    u8 *onchain_htlc_script = bitcoin_htlc_onchain_witness_script(
-        &chainparams->payment_basepoint,
-        &user_pubkey,
-        &chainparams->htlc_secretkey,
-        &chainparams->htlc_point,
-        locktime,
-        &hash,
-        HTLC_TIMEOUT
-    );
+  /* Construct the HTLC script for the on-chain transaction */
+  const onchainHtlcScript = bitcoin.script.compile([
+    bitcoin.opcodes.OP_IF,
+    bitcoin.script.number.encode(locktime),
+    bitcoin.opcodes.OP_CHECKLOCKTIMEVERIFY,
+    bitcoin.opcodes.OP_DROP,
+    userPublicKey,
+    bitcoin.opcodes.OP_ELSE,
+    bitcoin.opcodes.OP_SHA256,
+    hash,
+    bitcoin.opcodes.OP_EQUALVERIFY,
+    bitcoin.opcodes.OP_DUP,
+    bitcoin.opcodes.OP_HASH160,
+    bitcoin.crypto.hash160(userPublicKey),
+    bitcoin.opcodes.OP_ENDIF,
+    bitcoin.opcodes.OP_CHECKSIG,
+  ]);
 
-    /* Construct the HTLC output for the on-chain transaction */
-    struct bitcoin_tx_output onchain_tx_output = {
-        .amount = onchain_output,
-        .script = scriptpubkey_p2wsh(onchain_htlc_script)
-    };
+  /* Construct the HTLC output for the on-chain transaction */
+  const onchainTxOutput = {
+    value: onchainOutput,
+    script: bitcoin.script.witnessPubKeyHash.output.encode(
+      bitcoin.crypto.hash160(onchainHtlcScript)
+    ),
+  };
 
-    /* Construct the HTLC output for the Lightning network transaction */
-    struct bitcoin_tx_output lightning_tx_output = {
-        .amount = amount_msat_to_sat_round_down(lightning_output),
-        .script = scriptpubkey_p2wpkh(&user_pubkey)
-    };
+  /* Construct the HTLC output for the Lightning network transaction */
+  const lightningTxOutput = {
+    value: lightningOutput,
+    script: bitcoin.script.witnessPubKeyHash.output.encode(
+      bitcoin.crypto.hash160(userPublicKey)
+    ),
+  };
 
-    /* Generate a random multisig key for the swap */
-    struct pubkey swap_multisig_key;
-    if (!generate_keypair(&swap_multisig_key, NULL))
-        return 1;
+  /* Generate a random multisig key for the swap */
+  const swapMultisigKeyPair = bitcoin.ECPair.makeRandom({ network: chainparams });
+  const swapMultisigPublicKey = swapMultisigKeyPair.publicKey;
 
-    /* Construct the HTLC script for the Lightning network transaction */
-    u8 *lightning_htlc_script = bitcoin_htlc_success_tx(
-        &chainparams->payment_basepoint,
-        &swap_multisig_key,
-        &chainparams->htlc_secretkey,
-        &chainparams->htlc_point,
-        &hash,
-        HTLC_SUCCESS
-    );
+  /* Construct the HTLC script for the Lightning network transaction */
+  const lightningHtlcScript = bitcoin.script.compile([
+    bitcoin.opcodes.OP_IF,
+    swapMultisigPublicKey,
+    bitcoin.opcodes.OP_ELSE,
+    bitcoin.opcodes.OP_SHA256,
+    hash,
+    bitcoin.opcodes.OP_EQUALVERIFY,
+    userPublicKey,
+    bitcoin.opcodes.OP_ENDIF,
+    bitcoin.opcodes.OP_CHECKSIG,
+  ]);
 
-    /* Construct the multisig output for the Lightning network transaction */
-    struct bitcoin_tx_output lightning_multisig_output = {
-        .amount = amount_msat_to_sat_round_down(lightning_output),
-        .script = scriptpubkey_p2wsh(scriptpubkey_2of2(
-            &swap_multisig_key, &user_pubkey))
-    };
+  /* Construct the multisig output for the Lightning network transaction */
+  const lightningMultisigOutput = {
+    value: lightningOutput,
+    script: bitcoin.script.witnessScriptHash.output.encode(
+      bitcoin.crypto.hash160(
+        bitcoin.script.compile([
+          bitcoin.opcodes.OP_2,
+          swapMultisigPublicKey,
+          userPublicKey,
+          bitcoin.opcodes.OP_2,
+          bitcoin.opcodes.OP_CHECKMULTISIG,
+        ])
+      )
+    ),
+  };
 
-    /* Construct the transaction input for the on-chain transaction */
-    struct bitcoin_tx_input onchain_tx_input = {
-        .txid = { .u
+  /* Construct the transaction input for the on-chain transaction */
+  const onchainTxInput = {
+    hash: Buffer.from('...', 'hex'), // Specify the previous transaction hash
+    index: 0, // Specify the previous transaction output index
+    script: Buffer.from('...', 'hex'), // Specify the previous output's script
+    witness: [], // Specify the script's witness data
+  };
+
+  /* Continue with the Bitcoin-specific logic for constructing the transactions */
+}
+
+main().catch(console.error);
