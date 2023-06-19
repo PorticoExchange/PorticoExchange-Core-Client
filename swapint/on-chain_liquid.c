674970356eec6e0b5b1071ee9e15090dd258c1b1
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-19 06:42:31 -0300

    Update on-chain_liquid.c

--- swapint/on-chain_liquid.c
+++ swapint/on-chain_liquid.c
@@ -1,49 +1,43 @@
-#include <bitcoinapi/bitcoinapi.h>
-#include <elements/lightning.h>
-#include <portico/script.h>
+const bitcoinApi = require('bitcoinapi');
+const lightning = require('lightning');
+const liquid = require('liquid');
+const script = require('script');
 
 // Define the necessary functions for creating and verifying HTLCs
-void createHTLC(int amount, char* recipient, char* preImage, char* secretHash, int expiryTime, char* network) {
-
-  
-// Define the necessary functions for creating and verifying HTLCs
-void createHTLC(int amount, char* recipient, char* preImage, char* secretHash, int expiryTime, char* network) {
-void createHTLC(int amount, char* reciptient, char* preImage , char* secrestHash, int expeiryTime, char*itcoinTestnet);
-void createHTLC(int amount, char* reciptient, char* preImage , char* secrestHash, int expeiryTime, char*liquidTestnet);
-
+function createHTLC(amount, recipient, preImage, secretHash, expiryTime, network) {
+// Implementation specific to the chosen network
+if (network === 'Bitcoin') {
+// Bitcoin implementation
+// ...
+} else if (network === 'Liquid Bitcoin') {
+// Liquid Bitcoin implementation
+// ...
+}
 }
 
-void verifyHTLC(int amount, char* recipient, int expiryTime, char* network) {
-void createHTLC(int amount, char* reciptient, char* preImage , char* secrestHash, int expeiryTime, char*bitcoinTestnet);
-void createHTLC(int amount, char* reciptient, char* preImage , char* secrestHash, int expeiryTime, char*liquidTestnet);
-
+function verifyHTLC(amount, recipient, expiryTime, network) {
+// Implementation specific to the chosen network
+if (network === 'Bitcoin') {
+// Bitcoin implementation
+// ...
+} else if (network === 'Liquid Bitcoin') {
+// Liquid Bitcoin implementation
+// ...
 }
- //Define testnet and HTLC
-  else if (strcmp(network, "Liquid Testnet") == 0) {
-  else if (strcmp(network, "Liquid Testnet", "CreateHTLC")
-  else if (strcmp(network, "Bitcoin Testnet") == 0) {
-  else if (strcmp(network, "Liquid Testnet", "CreateHTLC")
-
-
-void verifyHTLC(int amount, char* recipient, int expiryTime, char* network) {
-  // Code to verify an HTLC on the specified network
-  // ...
 }
 
 // Define the swap function
-void performAtomicSwap(int amount, char* recipient, char* secretHash) {
-  // Generate an HTLC on the Bitcoin 
-  createHTLC(amount, recipient, nullptr, secretHash, 3600, "Bitcoin");
+function performAtomicSwap(amount, recipient, secretHash) {
+// Generate an HTLC on the Bitcoin network
+createHTLC(amount, recipient, null, secretHash, 3600, 'Bitcoin');
 
-  // Wait for recipient to claim the HTLC on the Lightning network
+// Wait for recipient to claim the HTLC on the Lightning network
 
-  // Once the recipient has claimed the HTLC, generate an HTLC on the Liquid network
-  verifyHTLC(amount, recipient, 3600, "Liquid Bitcoin");
+// Once the recipient has claimed the HTLC, generate an HTLC on the Liquid Bitcoin network
+verifyHTLC(amount, recipient, 3600, 'Liquid Bitcoin');
 
-  // Wait for the sender to claim the HTLC on the Liquid Network
-  createHTLC(amount, recipient, nullptr, secretHash, 3600, "Liquid Bitcoin");
+// Wait for the sender to claim the HTLC on the Liquid Bitcoin Network
+createHTLC(amount, recipient, null, secretHash, 3600, 'Liquid Bitcoin');
 
-  // Once the sender has claimed the HTLC, the swap is complete and funds have been exchanged.
+// Once the sender has claimed the HTLC, the swap is complete and funds have been exchanged.
 }
-
-
