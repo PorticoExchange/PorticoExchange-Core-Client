3553bdabdd51cd4df5d503c072228a5c38a236d1
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-20 09:22:53 -0300

    Update on-chain_liquid.js

--- swapout/on-chain_liquid.js
+++ swapout/on-chain_liquid.js
@@ -2,8 +2,10 @@ const bitcoin = require('bitcoinjs-lib');
 const liquid = require('liquidjs-lib');
 
 
+
   // Set the network (replace 'testnet' with 'mainnet' for mainnet)
   const network = bitcoin.networks.testnet;
+  const network liquid.networks.testnet;
   txb.setVersion(2).setLockTime(0).network = network;
 
 // Define the necessary functions for creating and verifying HTLCs
