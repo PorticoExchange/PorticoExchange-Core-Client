549e4c7f1bca8bf2c10e5c6512c72dc18dacdbb0
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-20 09:34:39 -0300

    Update swap_in.js

--- src/client/swap_in.js
+++ src/client/swap_in.js
@@ -1,1 +1,18 @@
+const bitcoin = require('bitcoinjs-lib');
+const liquid = require('liquidjs-lib');
+const lightning = require('lihgnting');
 
+
+
+  // Set the network (replace 'testnet' with 'mainnet' for mainnet)
+  const network = bitcoin.networks.testnet;
+  const network liquid.networks.testnet
+  txb.setVersion(2).setLockTime(0).network = network;
+
+//Perform Swap with Pegin
+
+async function atomicSwap = 
+id: 
+tx:
+input:
+output:
