75f93bc435ead2a2f62421969bf3aecbe096ff00
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-20 09:18:17 -0300

    Update liquid_lightning.js

--- swapint/liquid_lightning.js
+++ swapint/liquid_lightning.js
@@ -5,6 +5,11 @@ const liquid = require('liquidjs-lib');
 const asset = require('asset');
 const script = require('script');
 
+
+  // Set the network (replace 'testnet' with 'mainnet' for mainnet)
+  const network = bitcoin.networks.testnet;
+  txb.setVersion(2).setLockTime(0).network = network;
+
 // Define the necessary functions for creating and verifying HTLCs
 function createHTLC(amount, recipient, preImage, secretHash, expiryTime, network) {
 // Implementation specific to the chosen network
